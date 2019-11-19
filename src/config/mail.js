7a84d678f2f78ce9d2859b146b385faa67bd9384
Joao Lucas <jlmelosouza18@gmail.com>
2019-11-18 21:21:29 -0300

    feat: environment variable

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '0fd604b1b90474',
-    pass: '3ed88affef6558',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://f59863df84934e0880d0337100664822@sentry.io/1825586',
+  dsn: process.env.SENTRY_DSN,
 };
